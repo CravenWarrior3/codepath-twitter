d01b775c957556fe5afb1102e3efc17231118953
Ethan Cooper <cravenwarrior3@gmail.com>
2022-10-03 22:28:30 -0400

    Part 2 assignment

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,6 +18,12 @@ class HomeTableViewController: UITableViewController {
         loadTweet()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadTweet()
+    }
+    
     func loadTweet() {
         let params = ["count": 10]
         
@@ -52,6 +58,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
